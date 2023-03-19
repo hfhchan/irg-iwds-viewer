452da7bb00e447f16d5596f435a562e7fdbf99c3
Henry Chan <hfhchan@users.noreply.github.com>
2023-03-20 03:08:41 +0800

    Use githack.com because of jsdelivr package limits

--- ucv.php
+++ ucv.php
@@ -337,11 +337,13 @@ function parseGroupSimp($group, $id, $id2 = '') {
 		$glyphs = explode(',', $entry->glyphs);
 		foreach ($glyphs as $glyph) {
 			if (strpos($glyph, 'u2ff') === 0 || true) {
-				echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.png" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+				// echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.png" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
 				// echo '<img src="https://raw.githack.com/kawabata/iwds/master/glyphs/'.html_safe($glyph).'.png" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+				echo '<img src="https://raw.githack.com/yi-bai/iwds/master/glyphs/'.html_safe($glyph).'.png" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
 			} else {
-				echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.svg" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+				// echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.svg" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
 				// echo '<img src="https://raw.githack.com/kawabata/iwds/master/glyphs/'.html_safe($glyph).'.svg" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+				echo '<img src="https://raw.githack.com/yi-bai/iwds/master/glyphs/'.html_safe($glyph).'.svg" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
 			}
 		}
 		echo '</a>';
@@ -396,9 +398,11 @@ function printEntry($entry) {
 	foreach ($glyphs as $glyph) {
 		echo "\t\t\t\t";
 		if (strpos($glyph, 'u2ff') === 0 || true) {
-			echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.png" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+			// echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.png" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+			echo '<img src="https://raw.githack.com/yi-bai/iwds/master/glyphs/'.html_safe($glyph).'.png" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
 		} else {
-			echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.svg" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+			// echo '<img src="https://cdn.jsdelivr.net/gh/yi-bai/iwds@master/glyphs/'.html_safe($glyph).'.svg" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
+			echo '<img src="https://raw.githack.com/yi-bai/iwds/master/glyphs/'.html_safe($glyph).'.svg" width=50 height=50 class=glyph title="'.html_safe($glyph).'" loading=lazy>';
 		}
 		echo "\r\n";
 	}
